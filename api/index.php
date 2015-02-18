6ef5a29181ebd9f8861dae1069e1d88ce05a4116
cbarretto7 <Claire@Claires-MacBook-Pro.local>
2015-02-17 23:28:49 -0800

    Database config

--- api/index.php
+++ api/index.php
@@ -10,27 +10,25 @@ $app->get('/students', 'getStudents');
 $app->run();
 
 function getStudents() {
-    echo "hellO";
-    // $sql = "SELECT * FROM STUDENT";
-
-    // try {
-    //     $db = getConnection();
-    //     $stmt = $db->query($sql);
-    //     $students = $stmt->fetchAll(PDO::FETCH_OBJ);
-    //     $db = null;
-    //     echo json_encode($students);
-    // } catch(PDOException $e) {
-    //     echo '{"error":{"text":'. $e->getMessage() .'}}';
-    // }
+    $sql = "SELECT * FROM STUDENT";
+    try {
+        $db = getConnection();
+        $stmt = $db->query($sql);
+        $students = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($students);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
 }
 
 /* 
  * TODO: put the credentials in a separate config file
  */
 function getConnection() {
-    $dbhost = "127.8.166.2";
-    $dbuser = "adminlPXC8Ak";
-    $dbpass = "VjipbM648C6c";
+    $dbhost = "127.4.196.130";
+    $dbuser = "adminpVaqD1a";
+    $dbpass = "GpFqpeavU2dT";
     $dbname = "gobind";
     $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);  
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
