24165ccffe8ea0876c25069a3b2e9113e3dfc2ac
Shanifer Seit <shanifer@gmail.com>
2015-03-02 20:18:58 -0800

    added search for student and users

--- api/index.php
+++ api/index.php
@@ -6,82 +6,185 @@ require 'Slim/Slim.php';
 $app = new \Slim\Slim();
 
 $app->get('/students', 'getStudents');
-$app->get('/teachers', 'getTeachers');
 $app->get('/students/:id', 'getStudentById');
 $app->post('/students', 'createStudent');
 $app->put('/students/:id', 'updateStudent');
 $app->delete('/students/:id', 'deleteStudent');
 
+$app->get('/teachers', 'getTeachers');
+$app->get('/teachers/:id', 'getTeachersById');
+
+$app->get('/search/:usertype', 'findUsersByName');
+$app->get('/search/student', 'findStudents');
+
 $app->run();
 
 
-/* 
- * Returns a list of students
- */
+#================================================================================================================#
+# Students
+#================================================================================================================#
 function getStudents() {
-    $sql = "select s.id, s.firstName, s.lastName from student s";
+    $sql = "SELECT * from student order by firstName asc ";
     echo json_encode(perform_query($sql, 'GETALL'));
 }
 
-/* 
- * Returns a list of students
- */
-function getTeachers() {
-    $sql = "select t.id, t.firstName, t.lastName from teacher t";
-    echo json_encode(perform_query($sql, 'GETALL'));
-}
-/* 
- * Returns a single student record
- */
 function getStudentById($id) {
-    $sql = "select s.id, s.firstName, s.lastName, s.email from student s where s.id=:id";
+    $sql = "SELECT * from student where userid=:id";
     $bindparam = ["id"=>$id];
-    echo json_encode(perform_query($sql,'GET',$bindparam)
+    echo json_encode(perform_query($sql,'GET',$bindparam));
 }
 
-/* 
- * Updates a student record
- */
 function updateStudent($id) {
     $request = \Slim\Slim::getInstance()->request();
     $body = $request->getBody();
     $student = json_decode($body);
-    $sql = "update student set firstName=:firstName, lastName=:lastName, email=:email, id=:id WHERE id=:id";
+    $sql = "UPDATE student set firstName=:firstName, lastName=:lastName, email=:email, id=:id WHERE id=:id";
     $bindparams = [
         "firstName"=>$student->firstName,
-        "lastName", $student->lastName,
-        "email", $student->email,
-        "id", $student->id,
+        "lastName" => $student->lastName,
+        "email" => $student->email,
+        "id" => $student->id,
     ];
     perform_query($sql,'',$bindparams);
 }
 
-/* 
- * Creates a student record
- */
 function createStudent() {
     $request = \Slim\Slim::getInstance()->request();
     $body = $request->getBody();
     $student = json_decode($body);
-    $sql = "insert into student (id, firstName, lastName, email) values (:id, :firstName, :lastName, :email)";
+    $sql = "INSERT into student (userid, firstName, lastName, emailAddr) values (:userid, :firstName, :lastName, :emailAddr)";
     $bindparams = [
-        "firstName"=>$student->firstName,
-        "lastName", $student->lastName,
-        "email", $student->email,
-        "id", $student->id,
+        "firstName" => $student->firstName,
+        "lastName" => $student->lastName,
+        "email" => $student->emailAddr,
+        "userid" => $student->userid,
     ];
-    perform_query($sql,'',$bindparams);
+    echo json_encode(perform_query($sql,'POST',$bindparams));
 }
-
-/* 
- * Updates a student record
- */
+ 
 function deleteStudent($id) {
-    $sql = "delete from student where id=:id";
+    $sql = "DELETE from student where id=:id";
     $bindparam = ["id"=>$id];
-    perform_query($sql,'',$bindparam)
+    perform_query($sql,'',$bindparam);
 }
 
+
+function findStudents(){
+    $firstname = $_GET['firstName'];
+    $lastname = $_GET['lastName'];
+    $dob = $_GET['dateOfBirth'];
+    $gender = $_GET['gender'];
+
+    $bindparam = array();
+    $clause = '';
+    if (!(isset($firstname)||isset($lastname))){
+        echo 'must provide either first ane last name';
+    }
+    if (isset($firstname)) {
+        $clause."firstName like :firstname";
+        $firstname = "%".$firstname."%";
+        $bindparam["firstname"] = $firstname;
+        if (isset($lastname)){
+            $clause."and lastName like :lastname";
+            $lastname = "%".$lastname."%";
+            $bindparam["lastName"] = $lastname;
+        }
+    }
+    
+    if (isset($lastname)){
+        $clause."lastName like :lastname";
+        $lastname = "%".$lastname."%";
+        $bindparam["lastName"] = $lastname;
+    }
+
+
+    if (isset($dob)){
+        $clause." and dateOfBirth=:dob";
+        $bindparam["dob"] = $dob;
+    }
+    if (isset($gender)){
+        $clause." and gender=:gender";
+        $bindparam["gender"] = $gender;
+    }
+
+    $sql = "SELECT * from student where $clause and order by firstName asc";
+    echo json_encode(perform_query($sql,'GETALL',$bindparam));
+}
+
+
+#================================================================================================================#
+# Teachers
+#================================================================================================================#
+function getTeachers() {
+    $sql = "SELECT * from teacher order by firstName asc" ;
+    echo json_encode(perform_query($sql, 'GETALL'));
+}
+
+function getTeacherById($id) {
+    $sql = "SELECT * from teacher where userid=:id";
+    $bindparam = ["id"=>$id];
+    echo json_encode(perform_query($sql,'GET',$bindparam));
+}
+
+#================================================================================================================#
+# General find
+#================================================================================================================#
+function getUsers($type){
+    if ($type == "teacher"){
+        return getTeachers();
+    }
+    else if ($type == "student"){
+        return getStudents();
+    }
+    #TODO
+}
+
+function findUsersByName($usertype){
+    if (isset($_GET['firstName']) && isset($_GET['lastName'])) {
+        return findUsersByFullName($_GET['userType'],$_GET['firstName'], $_GET['lastName']);
+    }
+    else if (isset($_GET['firstName'])){
+        return findUsersByFirstName($_GET['userType'],$_GET['firstName']);
+    }  
+    else if (isset($_GET['lastName'])) {
+        return findUsers($_GET['userType'],$_GET['lastName']);
+    }
+    else {
+        return getUsers($_GET['userType']);
+    } 
+}
+
+function findUsersByFirstName($type, $query) {
+    $sql = "SELECT * from $type where firstName like :query order by firstName asc";
+    $query = "%".$query."%";
+    $bindparam = ["query"=>$query];
+    echo json_encode(perform_query($sql,'GETALL',$bindparam));
+}
+
+
+function findUsersByLastName($type, $query) {
+    $sql = "SELECT * from $type where lastName like :query ORDER BY lastName asc";
+    $query = "%".$query."%";
+    $bindparam = ["query"=>$query];
+    echo json_encode(perform_query($sql,'GETALL',$bindparam));
+}
+
+
+function findUsersByFullName($type, $query1, $query2) {
+    $sql = "SELECT * from $type where firstName like :query1 and lastName like :query2 order by firstName asc";
+    $query1 = "%".$query1."%";
+    $query2 = "%".$query2."%";
+    $bindparam = [
+        "query1" => $query1,
+        "query2" => $query2,
+        ];
+    echo json_encode(perform_query($sql,'GETALL',$bindparam));
+}
+
+
+#================================================================================================================#
+# Helpers
+#================================================================================================================#
 /* 
  * TODO: put the credentials in a separate config file
  */
@@ -109,21 +212,23 @@ function perform_query($sql, $querytype, $bindparams=array()) {
             }
             $stmt->execute();
         }
-
         if ($querytype == 'GET') {
             $result = $stmt->$fetchObject();
         }
         elseif ($querytype == 'GETALL') {
             $result = $stmt->fetchAll(PDO::FETCH_OBJ);
         } 
+        elseif ($querytype == 'POST'){
+            $result = $db->lastInsertId();
+        }
         else {
             $result = null;
         }
         $db = null;
         echo "success";
+        return $result;
 
     } catch(PDOException $e) {
         echo $e->getMessage();
     }
-    return $result;
 }
