f92b0b72aed4d18c4cd23d8060b26d3418accd36
coyox <alloudsounds@gmail.com>
2015-03-17 17:45:51 -0700

    Basic mandrill api calls implemented. Will currently send from MY EMAIL (no spam pls) to MY EMAIL...

--- js/views/email/email.js
+++ js/views/email/email.js
@@ -7,6 +7,9 @@ var EmailView = Backbone.View.extend({
 	render: function() {
 		this.$el.html(html["email.html"]);
 		//this.$el.find("#email-to").val(this.emailAddr);
+
+		//var userEmail = sessionStorage.getItem('gobind-email') ; // TODO: Get the email of the logged in user
+		//this.$el.find("#email-from").val(userEmail);
 	},
 
 	events: {
@@ -17,10 +20,40 @@ var EmailView = Backbone.View.extend({
 		var recipient = this.$el.find("#email-to").val();
 		var subject = this.$el.find("#email-subject").val();
 		var body = this.$el.find("#email-message").val();
-		
-		console.log(recipient);
-		console.log(body);
-		console.log(subject);
+		var apiKey = 'C_s6D7OmZEgKBIspAvuBcw'
+		var from = this.$el.find("#email-from").val();
+
+		$.ajax({
+  		type: 'POST',
+  		url: "https://mandrillapp.com/api/1.0/messages/send.json",
+  		data: {
+    	'key': apiKey,
+    	'message': {
+      	'from_email': 'alloyd@Live.ca', // Todo: replace with user's email
+      	'to': [
+        	  {
+           	 'email': 'adaml@Live.ca',
+           	 'name': 'Adam Lloyd',
+           	 'type': 'to'
+          	},
+          	//{
+           	// 'email': 'alloudsounds@gmail.com',
+           	// 'name': 'Adam Lloyd',
+           	// 'type': 'to'
+          	//}
+        	],
+      	'autotext': 'true',
+      	'subject': subject,
+      	'html': body,
+    	}
+  	}
+ 	}).done(function(response) {
+   	console.log(response);
+ 	});
+
+		//console.log(recipient);
+		//console.log(body);
+		//console.log(subject);
 	}
 
 });
