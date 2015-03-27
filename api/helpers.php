4c4f148fe6f6d4416731eb5c3abd955d5b07f69f
Shanifer Seit <shanifer@gmail.com>
2015-03-27 11:42:07 -0700

    comment out email

--- api/helpers.php
+++ api/helpers.php
@@ -44,33 +44,33 @@ function generateUniqueID($sql, $param, $digit=IDdigits){
     return $id;
 }
 
-function emailLogin($emailAdd, $username, $password, $firstname, $lastname){
-    $mandrill = new Mandrill('C_s6D7OmZEgKBIspAvuBcw');
-    try {
-        $message = array(
-        'html' => '<p>Example HTML content</p>',
-        'text' => 'hello',
-        'subject' => 'Welcome to Gobind Sarvar School',
-        'from_email'=>'shaniferseit@hotmail.com',
-        'from_name' => 'Gobind Sarvar School',
-        'to' => array(
-            array(
-                'email' => 'shanifer@gmail.com',
-                'name' => 'Shanifer',
-                'type' => 'to'
-            )
-        ),
-        );
-    $async = false;
-    $result = $mandrill->messages->send($message, $async);
-    print_r($result);
-} catch(Mandrill_Error $e) {
-    // Mandrill errors are thrown as exceptions
-    echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
-    // A mandrill error occurred: Mandrill_Unknown_Subaccount - No subaccount exists with the id 'customer-123'
-    throw $e;
-}
-}
+// function emailLogin($emailAdd, $username, $password, $firstname, $lastname){
+//     $mandrill = new Mandrill('C_s6D7OmZEgKBIspAvuBcw');
+//     try {
+//         $message = array(
+//         'html' => '<p>Example HTML content</p>',
+//         'text' => 'hello',
+//         'subject' => 'Welcome to Gobind Sarvar School',
+//         'from_email'=>'shaniferseit@hotmail.com',
+//         'from_name' => 'Gobind Sarvar School',
+//         'to' => array(
+//             array(
+//                 'email' => 'shanifer@gmail.com',
+//                 'name' => 'Shanifer',
+//                 'type' => 'to'
+//             )
+//         ),
+//         );
+//     $async = false;
+//     $result = $mandrill->messages->send($message, $async);
+//     print_r($result);
+// } catch(Mandrill_Error $e) {
+//     // Mandrill errors are thrown as exceptions
+//     echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
+//     // A mandrill error occurred: Mandrill_Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+//     throw $e;
+// }
+// }
 
 
 /*
