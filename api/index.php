05a68137a113bdb5a6d4ef0b820ecf1cc5e574c1
Claire Barretto <cbarretto7@gmail.com>
2015-03-27 15:33:10 -0700

    Testting sql

--- api/index.php
+++ api/index.php
@@ -1288,7 +1288,7 @@ function updateCourseCompetencies($id) {
         $sql = "UPDATE teacherCourseCompetency set level=:level".$i." where userid=:userid and deptid=:deptid".$i;
         array_push($queries, $sql);
     }
-    echo $sql;
+    echo $queries;
     echo json_encode(perform_transaction($sql, $bindparams));
 }
 
