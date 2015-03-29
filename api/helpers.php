589c7d90246e689c91c79058590e94475c9732e5
Shanifer Seit <shanifer@gmail.com>
2015-03-28 20:01:51 -0700

    quotes around col

--- api/helpers.php
+++ api/helpers.php
@@ -43,7 +43,7 @@ function generateUniqueID($sql, $param, $digit=IDdigits){
 }
 
 function getKey($name){
-    $sql = "SELECT key from key where name=:name";
+    $sql = "SELECT `key` from key where name=:name";
     return perform_query($sql, 'GETCOL', array("name"=>$name));
 }
 
