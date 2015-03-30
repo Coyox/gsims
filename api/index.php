8ce55b349a5fe426c233f8fa8aa986d8bbf98c38
Shanifer Seit <shanifer@gmail.com>
2015-03-30 05:02:47 -0700

    pass fix

--- api/index.php
+++ api/index.php
@@ -145,16 +145,38 @@ $app->run();
 function validateCredentials() {
     $password = $_GET['password'];
     if (isset($password)){
-        $sql = "SELECT password from login where username=:username LIMIT 1";
+        $sql = "SELECT * from login where username=:username LIMIT 1";
         $bindparam = array("username"=> $_GET['username']);
         $user = perform_query($sql, 'GET', $bindparam);
         if ($user->password === crypt($password, $user->password)) {
             $sql = "UPDATE login set lastLogin=CURRENT_TIMESTAMP where username=:username";
-            echo json_encode(perform_query($sql, '', $bindparam));
+            $resp = perform_query($sql, '', $bindparam);
+            if ($resp["status"]=="success"){
+                echo json_encode($user);
+            }
+            else {
+                echo $resp;
+            }
         }
         else {
             echo json_encode(array("status"=>"failure"));
         }
+
+
+
+        $sql = "SELECT * from login where username=:username LIMIT 1";
+        $bindparam = array("username"=> $_GET['username']);
+        $user = perform_query($sql, 'GET', $bindparam);
+        // if ( hash_equals($user->password, crypt($password, $user->password)) ) {
+        //     echo json_encode($user);
+        // }
+        $sql = "UPDATE login set lastLogin=CURRENT_TIMESTAMP where username=:username";
+        perform_query($sql, '', $bindparam);
+        echo json_encode($user);
+
+
+
+
     }
     else {
         $sql = "SELECT userid, username, usertype, lastLogin from login order by userid asc";
