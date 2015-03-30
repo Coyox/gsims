e1b482a52e71be68ab9ae8b9d1f87203f802ba8d
Shanifer Seit <shanifer@gmail.com>
2015-03-30 02:08:45 -0700

    added viewAllSuperusers

--- js/models/user.js
+++ js/models/user.js
@@ -223,9 +223,35 @@ var Teacher = Backbone.Model.extend({
 
 var Superuser = Backbone.Model.extend({
 	defaults: {
+		userid: "",
+		firstName: "",
+		lastName: "",
 		emailAddr: "",
-		status: ""
+		status: "",
+		lastAccessed: ""
 	},
+
+	validation: {
+		firstName: {
+			required: true
+		},
+		lastName: {
+			required: true
+		},
+		emailAddr: [{
+			required: true
+		}, {
+			pattern: "email",
+			msg: "Please provide a valid email address."
+		}],
+		status: {
+			required: true
+		}
+	},
+	nonEditable: [
+   		"lastAccessed", "userid"
+   	],
+
     urlRoot: app.serverUrl + "api/superusers",
 });
 
--- js/views/superusers/viewSuperusers.js
+++ js/views/superusers/viewSuperusers.js
@@ -8,4 +8,126 @@ var SuperusersTableView = Backbone.View.extend({
 		storeContent();
 
 		this.$el.html(html["viewSuperusers.html"]);
-	}});
+		if (this.results) {
+			this.populateQueryResults(this.results);
+		} else {
+			this.fetchAllResults();
+		}
+	},
+
+	events: {
+		"click #refresh": "refreshTable",
+		"click .send-email": "openEmailModal",
+		"change .toggle-checkboxes": "toggleCheckboxes"
+	},
+
+	populateQueryResults: function(data) {
+		_.each(data, function(object, index) {
+			var model = new Superuser(object, {parse:true});
+			new SuperuserTableRowView({
+				model: model,
+				el: this.addRow(".results", model.get("emailAddr"))
+			});
+		}, this);
+		view.table = this.$el.find("table").dataTable({
+	      	aoColumnDefs: [
+	          	{ bSortable: false, aTargets: [ 4, 5 ] },
+	          	{ sClass: "center", aTargets: [ 4, 5 ] },
+	          	{ sWidth: "10%", aTargets: [ 5 ] }
+	       	]
+		});
+		this.$el.find(".dataTables_filter").append("<button class='send-email btn btn-sm btn-primary dt-btn'>Send Email</button>");
+		this.$el.find(".dataTables_filter").append("<button id='refresh' class='btn btn-sm btn-primary dt-btn'>Refresh Table</button>");
+	},
+
+
+fetchAllResults: function() {
+		var view = this;
+		var model = new Superuser();
+		model.fetch({
+			url: model.urlRoot,
+		}).then(function(data) {
+			_.each(data, function(object, index) {
+				var model = new Superuser(object, {parse:true});
+				new SuperuserTableRowView({
+					model: model,
+					el: view.addRow(".results", model.get("emailAddr"))
+				});
+			}, view);
+			view.table = view.$el.find("table").dataTable({
+		      	aoColumnDefs: [
+		          	{ bSortable: false, aTargets: [ 4, 5 ] },
+		          	{ sClass: "center", aTargets: [ 4, 5 ] },
+		          	{ sWidth: "10%", aTargets: [ 5 ] }
+		       	]
+			});
+			view.$el.find(".dataTables_filter").append("<button class='send-email btn btn-sm btn-primary dt-btn'>Send Email</button>");
+			view.$el.find(".dataTables_filter").append("<button id='refresh' class='btn btn-sm btn-primary dt-btn'>Refresh</button>");
+		});
+	},
+
+	addRow: function(selector, email) {
+        var container = $("<tr></tr>");
+        container.data("email", email);
+        this.$el.find(selector).first().append(container);
+        return container;
+	},
+
+	openEmailModal: function(evt) {
+		var recipients = [];
+		_.each(this.table.fnGetNodes(), function(row, index) {
+			var checkbox = $(row).find("input[type='checkbox']");
+			if ($(checkbox).is(":checked")) {
+				recipients.push($(checkbox).closest("tr").data("email"));
+			}
+		}, this);
+
+		var numRecipients = recipients.length;
+		openEmailModal(recipients, numRecipients, "S");
+	},
+
+	toggleCheckboxes: function(evt) {
+		var nodes = this.table.fnGetNodes();
+		var checked = $(evt.currentTarget).is(":checked");
+		_.each(nodes, function(row, index) {
+			var checkbox = $(row).find("input[type='checkbox']");
+			checkbox.prop("checked", checked);
+		}, this);
+	},
+
+	refreshTable: function(evt) {
+		evt.stopImmediatePropagation();
+		this.table.fnDestroy();
+		this.render();
+	},
+});
+
+var SuperuserTableRowView = Backbone.View.extend({
+	template: _.template("<td><%= model.userid %></td>"
+		+	"<td><%= model.firstName %></td>"
+		+	"<td><%= model.lastName %></td>"
+		+	"<td><%= model.emailAddr %></td>"
+		+   "<td><span class='view-superuser primary-link center-block' id='<%= model.userid %>'>[ View Superuser ]</span></td>"
+		+	"<td><input type='checkbox' class='user-row' checked></td>"),
+
+	initialize: function(options) {
+		this.render();
+	},
+
+	render: function() {
+		this.$el.html(this.template({
+			model: this.model.toJSON()
+		}));
+	},
+
+	events: {
+		"click .view-superuser": "viewSuperuser",
+	},
+
+	viewAdmin: function(evt) {
+		storeContent();
+
+		var id = $(evt.currentTarget).attr("id");
+		app.Router.navigate("superusers/" + id, {trigger:true});
+	}
+});
