22de86356c05f83b335081ea01c32680979302bd
Shanifer Seit <shanifer@gmail.com>
2015-04-02 23:23:13 -0700

    fixed quotes to be outside of like operator

--- api/helpers.php
+++ api/helpers.php
@@ -212,7 +212,7 @@ function buildWhereClause($fields){
     foreach ($fields as $key=>$value) {
         $clause.= " AND ";
         if(substr($key, -4) === 'Name'){
-            $clause.=$key." like %:".$key."%";
+            $clause.=$key." like '%:".$key."%'";
         }
         else {
             $clause.=$key."=:".$key;
