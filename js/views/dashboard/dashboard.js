e3ca848182de849ffb75a6734bf887762b7260e8
Shanifer Seit <shanifer@gmail.com>
2015-04-03 23:51:41 -0700

    updated calendar

--- js/views/dashboard/dashboard.js
+++ js/views/dashboard/dashboard.js
@@ -4,7 +4,12 @@ var DashboardView = Backbone.View.extend({
 	},
 
 	render: function() {
-		this.$el.html(html["dashboard.html"]);
+		var template = html["dashboard.html"];
+		template = template({
+			usertype: sessionStorage.getItem("gobind-usertype")
+		});
+		this.$el.html(template);
+
 
 		var usertype = sessionStorage.getItem("gobind-usertype");
 
@@ -19,7 +24,7 @@ var DashboardView = Backbone.View.extend({
 
 		// Calendar
 		if (usertype == "SU" || usertype == "A" || usertype == "T") {
-			this.calendarWidget();
+			this.calendarWidget(usertype);
 		}
 	},
 
@@ -121,13 +126,111 @@ var DashboardView = Backbone.View.extend({
 		});
 	},
 
-	calendarWidget: function() {
+	calendarWidget: function(usertype) {
 		var view = this;
-		var sections = [];
-		var section = new Section();
-
 		var mon = [], tue = [], wed = [], thu = [], fri = [], sat = [], sun = [];
 
+		if (usertype == "T") {
+			var user = JSON.parse(sessionStorage.getItem("gobind-user"));
+			var userid = user.userid;
+			var teacher = new Teacher();
+			teacher.fetch({
+				url: teacher.getTeachingSectionsUrl(userid)
+			}).then(function(data){
+				// Get all sections
+				_.each(data, function(section, index) {
+				// Get all days for a section
+				var days = section.day.split(",");
+				_.each(days, function(day, index) {
+					var obj = {
+						start: section.startTime,
+						end: section.endTime,
+						courseName: section.courseName,
+					};
+					switch (day) {
+						case "MON":
+						mon.push(obj);
+						break;
+						case "TUE":
+						tue.push(obj);
+						break;
+						case "WED":
+						wed.push(obj);
+						break;
+						case "THU":
+						thu.push(obj);
+						break;
+						case "FRI":
+						fri.push(obj);
+						break;
+						case "SAT":
+						sat.push(obj);
+						break;
+						case "SUN":
+						sun.push(obj);
+						break;
+						default:
+						break;
+					}
+				});
+			});
+
+				view.$el.find("#calendar").fullCalendar({
+					header: {
+						left: 'prev,next today',
+						center: 'title',
+						right: 'month,agendaWeek,agendaDay'
+					},
+					defaultView: "agendaWeek",
+					editable: false,
+					eventRender: function(event, element) {
+     			 		element.popover({
+     			 			title: event.title,
+     			 			placement:'auto',
+     			 			html:true,
+     			 			trigger : 'click',
+     			 			animation : 'true',
+     			 			content: event.description,
+     			 			container:'body'
+     			 		});
+     			 		$('body').on('click', function (e) {
+     			 			if (!element.is(e.target) && element.has(e.target).length === 0 && $('.popover').has(e.target).length === 0)
+     			 				element.popover('hide');
+     			 		});
+     				}
+
+     				});
+
+				view.$el.find("#calendar").fullCalendar( 'addEventSource',
+					function(start, end, status, callback) {
+						var events = [];
+
+						for (loop = start._d.getTime();
+							loop <= end._d.getTime();
+							loop = loop + (24 * 60 * 60 * 1000)) {
+							var test_date = new Date(loop);
+
+							var day = test_date.is().monday()? mon: test_date.is().tuesday()? tue : test_date.is().wednesday()? wed : test_date.is().thursday()? thu : test_date.is().friday()? fri : test_date.is().saturday()? sat : sun;
+							_.each(day, function(section, index) {
+								events.push({
+									title: section.courseName,
+									start: view.getStartTime(test_date, section),
+									end: view.getEndTime(test_date, section),
+									description: section.description,
+									//sectionCode: section.sectionCode
+
+							});
+						});
+					}
+					callback( events );
+				}
+				);
+			});
+
+	}
+	else {
+
+		var section = new Section();
 		section.fetch({
 			data: {
 				schoolid: sessionStorage.getItem("gobind-schoolid"),
@@ -146,142 +249,96 @@ var DashboardView = Backbone.View.extend({
 					};
 					switch (day) {
 						case "MON":
-							mon.push(obj);
-							break;
+						mon.push(obj);
+						break;
 						case "TUE":
-							tue.push(obj);
-							break;
+						tue.push(obj);
+						break;
 						case "WED":
-							wed.push(obj);
-							break;
+						wed.push(obj);
+						break;
 						case "THU":
-							thu.push(obj);
-							break;
+						thu.push(obj);
+						break;
 						case "FRI":
-							fri.push(obj);
-							break;
+						fri.push(obj);
+						break;
 						case "SAT":
-							sat.push(obj);
-							break;
+						sat.push(obj);
+						break;
 						case "SUN":
-							sun.push(obj);
-							break;
+						sun.push(obj);
+						break;
 						default:
-							break;
+						break;
 					}
 				});
 			});
 
 			view.$el.find("#calendar").fullCalendar({
-		        header: {
-		            left: 'prev,next today',
-		            center: 'title',
-		            right: 'month,agendaWeek,agendaDay'
-		        },
+				header: {
+					left: 'prev,next today',
+					center: 'title',
+					right: 'month,agendaWeek,agendaDay'
+				},
 				defaultView: "agendaWeek",
-				editable: true
+				editable: false,
+				eventRender: function(event, element) {
+     			 	element.popover({
+     			 		title: event.title,
+     			 		placement:'auto',
+     			 		html:true,
+     			 		trigger : 'click',
+     			 		animation : 'true',
+     			 		content: event.description,
+     			 		container:'body'
+     			 	});
+     			 	$('body').on('click', function (e) {
+     			 		if (!element.is(e.target) && element.has(e.target).length === 0 && $('.popover').has(e.target).length === 0)
+     			 			element.popover('hide');
+     			 	});
+     			}
 			});
 
-		    view.$el.find("#calendar").fullCalendar( 'addEventSource',        
-		        function(start, end, status, callback) {
-		            var events = [];
+			view.$el.find("#calendar").fullCalendar( 'addEventSource',
+				function(start, end, status, callback) {
+					var events = [];
 
-		            for (loop = start._d.getTime();
-	 					 loop <= end._d.getTime();
-		                 loop = loop + (24 * 60 * 60 * 1000)) {
+					for (loop = start._d.getTime();
+						loop <= end._d.getTime();
+						loop = loop + (24 * 60 * 60 * 1000)) {
+						var test_date = new Date(loop);
+					var day = test_date.is().monday()? mon: test_date.is().tuesday()? tue : test_date.is().wednesday()? wed : test_date.is().thursday()? thu : test_date.is().friday()? fri : test_date.is().saturday()? sat : sun;
+					_.each(day, function(section, index) {
+						events.push({
+							title: section.courseName,
+							start: view.getStartTime(test_date, section),
+							end: view.getEndTime(test_date, section),
+							description: section.description,
+						});
+					});
+				}
 
-		                var test_date = new Date(loop);
-		                if (test_date.is().monday()) {
-		                	_.each(mon, function(section, index) {
-		                		events.push({
-		                			title: section.courseName,
-		                			start: view.getStartTime(test_date, section),
-		                			end: view.getEndTime(test_date, section)
-		                		});
-		                	});
-		                }
-
-		                if (test_date.is().tuesday()) {
-		                	_.each(tue, function(section, index) {
-		                		events.push({
-		                			title: section.courseName,
-		                			start: view.getStartTime(test_date, section),
-		                			end: view.getEndTime(test_date, section)
-		                		});
-		                	});
-		                }
-
-		                if (test_date.is().wednesday()) {
-		                	_.each(wed, function(section, index) {
-		                		events.push({
-		                			title: section.courseName,
-		                			start: view.getStartTime(test_date, section),
-		                			end: view.getEndTime(test_date, section)
-		                		});
-		                	});
-		                }
-
-		                if (test_date.is().thursday()) {
-		                	_.each(thu, function(section, index) {
-		                		events.push({
-		                			title: section.courseName,
-		                			start: view.getStartTime(test_date, section),
-		                			end: view.getEndTime(test_date, section)
-		                		});
-		                	});             	
-		                }
-
-		                if (test_date.is().friday()) {
-		                	_.each(fri, function(section, index) {
-		                		events.push({
-		                			title: section.courseName,
-		                			start: view.getStartTime(test_date, section),
-		                			end: view.getEndTime(test_date, section)
-		                		});
-		                	});	                	
-		                }
-
-		                if (test_date.is().saturday()) {
-		                	_.each(sat, function(section, index) {
-		                		events.push({
-		                			title: section.courseName,
-		                			start: view.getStartTime(test_date, section),
-		                			end: view.getEndTime(test_date, section)
-		                		});
-		                	});	                	
-		                }
-
-		                if (test_date.is().sunday()) {
-		                	_.each(sun, function(section, index) {
-		                		events.push({
-		                			title: section.courseName,
-		                			start: view.getStartTime(test_date, section),
-		                			end: view.getEndTime(test_date, section)
-		                		});
-		                	});                	
-		                }
-
-		            }
-
-		            callback( events );
-		        }
-		    );
+				callback( events );
+			}
+			);
 		});
-	},
+}
+},
 
-	getStartTime: function(test_date, section) {
-		var start = new Date(test_date.getTime());
-		var str = section.start.split(":");
-		start.setHours(str[0]);
-		start.setMinutes(str[1]);
-		return start;
-	},
+getStartTime: function(test_date, section) {
+	var start = new Date(test_date.getTime());
+	var str = section.start.split(":");
+	start.setHours(str[0]);
+	start.setMinutes(str[1]);
+	return start;
+},
 
-	getEndTime: function(test_date, section) {
-		var end = new Date(test_date.getTime());
-		var str = section.end.split(":");
-		end.setHours(str[0]);
-		end.setMinutes(str[1]);
-		return end;
-	}
+getEndTime: function(test_date, section) {
+	var end = new Date(test_date.getTime());
+	var str = section.end.split(":");
+	end.setHours(str[0]);
+	end.setMinutes(str[1]);
+	return end;
+}
 });
