1c72db648910d16b333103658ffe95275d8ffc7f
i4c8 <i4c8@ugrad.cs.ubc.ca>
2015-04-05 20:22:13 -0700

    Progress on My Courses page. (database rebuilt need to make new test data)

--- js/views/school/myCourses.js
+++ js/views/school/myCourses.js
@@ -1,14 +1,46 @@
 var MyCoursesView = Backbone.View.extend({
     initialize: function (options) {
-        this.model = new Dept();
+        this.model = new Section();
         this.render();
+        //console.log(user["userid"]);
     },
      
     events: {
         
     },
+
      render: function () {
+        var view = this;
         this.$el.html(html["myCourses.html"]);
+        var teacher = new Teacher();
+        var user = JSON.parse(sessionStorage.getItem("gobind-user"));
+        var userid = user["userid"];
+        teacher.fetch({
+            url: teacher.getTeachingSectionsUrl(userid)
+        }).then(function(data){
+            console.log(data);
+            if (data.length == 0) {
+                var table = view.$el.find("#sections-table-container");
+                table.hide();
+                table.after("<br><div class='alert alert-danger'>This teacher is not assigned to any sections.</div>");
+            }
+            _.each(data, function(sec, index) {
+                var section = new Section(sec, {parse:true});
+               new CourseSectionRowView({
+                    el: view.addCourseSection(),
+                    model: section
+                })
+                console.log(section);
+            });
+        });
+        
     },
+
+    addCourseSection: function() {
+        var container = $("<tr></tr>");
+        this.$el.find("#sections-list tbody").append(container);
+        return container;
+    },
+
 
 });
