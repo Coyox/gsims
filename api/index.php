57e03030c9aed9a56689d3fd808024c1d490e8eb
Shanifer Seit <shanifer@gmail.com>
2015-04-10 07:52:41 -0700

    fixed require order

--- api/index.php
+++ api/index.php
@@ -1,14 +1,14 @@
 <?php
 require 'Slim/Slim.php';
 require_once 'crossdomain.php';
+require_once 'routes/user.php';
+require_once 'routes/school.php';
+require_once 'routes/function.php';
+
 
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
-
-require_once 'routes/user.php';
-require_once 'routes/school.php';
-require_once 'routes/function.php';
 
 $app->run();
 
